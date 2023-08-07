368c12c3e006d86926248a0b835c96b16e87ea5c
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-08-07 17:38:46 +0500

    get all records method for Drivers has been added

---
+++ MongDbWebAPI/Services/DriverService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using MongDbWebAPI.Configration;
+using MongDbWebAPI.Models;
+using MongoDB.Driver;
+
+namespace MongDbWebAPI.Services
+{
+    public class DriverService
+    {
+        private readonly IMongoCollection<Driver> _driverCollection;
+
+        public DriverService(IOptions<DatabaseSettings> options)
+        {
+            // start mongoDb client
+            var mongoClient = new MongoClient(options.Value.ConnectionString);
+            // connect to mongo database
+            var mongoDb = mongoClient.GetDatabase(options.Value.DatabaseName);
+            _driverCollection = mongoDb.GetCollection<Driver>(options.Value.CollectionName);
+        }
+
+        // retrieve all records
+        public async Task<List<Driver>> GetAsync()
+        {
+            return await _driverCollection.Find(_ => true).ToListAsync();
+        }
+    }
+}
